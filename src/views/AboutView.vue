49c4364202eeadccfe6f540ed3cfe3e9c111f9db
정수민 <jungsoomin@jeongsumin-ui-MacBookAir.local>
2024-06-02 16:49:39 +0900

    모바일 청첩장 프로젝트 시작

---
+++ src/views/AboutView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <h1>수민이와 선우의 모바일 웨딩 청첩장 이에요</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
